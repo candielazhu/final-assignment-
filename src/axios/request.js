406bb4e242a5179159fb90676a06906113a96bb9
“candielazhu” <candie_lazhu@163.com>
2025-12-21 08:44:46 +0800

    build: 更新依赖配置和服务器地址
    更新vite配置以支持CommonJS模块转换
    修改axios请求地址为生产环境IP
    升级多个依赖包版本

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://localhost:3000/api',  // 恢复/api前缀，与后端路由匹配
+  baseURL: 'http://47.115.214.161/api',  // 恢复/api前缀，与后端路由匹配
   timeout: 10000,
 })
 export default request
